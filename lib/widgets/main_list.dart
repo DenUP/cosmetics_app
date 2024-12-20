5fc399c80eebb7e1f5469ae7a7e305347f67a4e0
DenUP <ctrike163@gmail.com>
2024-12-20 10:54:33 +0300

    add: slider

--- lib/widgets/main_list.dart
+++ lib/widgets/main_list.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 
-class MainList extends StatelessWidget {
+class MainList extends StatefulWidget {
   const MainList({super.key});
 
   @override
+  State<MainList> createState() => _MainListState();
+}
+
+int _currentPage = 0;
+List<String> _imgScroll = [
+  'assets/img/slider/lineRepair.png',
+  'assets/img/slider/christinaMuse.png'
+];
+
+class _MainListState extends State<MainList> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 1,
-            itemBuilder: (context, index) {
-              Image.asset('assets/img/slider/christinaMuse.png');
-            },
-          )
-        ],
+      body: SizedBox(
+        height: 360,
+        child: PageView.builder(
+          itemCount: _imgScroll.length,
+          onPageChanged: (value) {
+            setState(() {
+              _currentPage = value;
+            });
+          },
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(_imgScroll[index]), fit: BoxFit.cover)),
+            );
+          },
+        ),
       ),
-    ));
+    );
   }
 }
